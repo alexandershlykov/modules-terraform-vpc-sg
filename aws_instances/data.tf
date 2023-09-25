b7a92e4fda8e85438049c835cbe73c4ed5a01dac
alexandershlykov <44493153+alexandershlykov@users.noreply.github.com>
2023-09-25 10:37:23 +0200

    Delete aws_instances directory

--- aws_instances/data.tf
+++
@@ -1,8 +0,0 @@
-data "aws_ami" "latest_amazon_linux" {
-  owners      = ["137112412989"]
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
-  }
-}
